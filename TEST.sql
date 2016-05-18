e354be41446cbee087918c1bcffc869a90de5aee
imargon <skypz666@gmail.com>
2016-05-18 11:33:25 +0800

    Add files via upload

--- TEST.sql
+++ TEST.sql
@@ -1,5 +1,5 @@
-﻿select * from ods.abpf10 t where t.ab10name='杨怡';
-select t1.rowid,t1.* from ods.capf10 t1 where t1.ca10name = '杨怡';
+﻿select * from ods.abpf10 t where t.ab10name='彭朕';
+select t1.rowid,t1.* from ods.capf10 t1 where t1.ca10name = '彭朕';
 
 select userenv('language') from dual;
 select * from sys.nls_database_parameters;
@@ -8,7 +8,7 @@ select * from sys.nls_session_parameters;
 SELECT * FROM V$NLS_PARAMETERS;
 
 select * from ods.plns_t_fact_loan;
-select * from ods.ulns_customer_info t2 where t2.customername = '杨怡';
+select * from ods.ulns_customer_info t2 where t2.customername = '彭朕';
 
 
 select * from sys.dbms_lob;
